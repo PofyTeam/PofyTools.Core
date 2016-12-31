eb5bfdad71e3c5063ea6535de260accc06ab4f7c
Studio Guvernal <contact@guvernal.com>
2016-12-31 17:33:41 +0100

    added extensions

---
+++ Extensions/EventTriggerExtensions.cs
@@ -0,0 +1,23 @@
+﻿
+namespace PofyTools
+{
+	using System;
+	using System.Collections;
+	using UnityEngine;
+	using UnityEngine.Events;
+	using UnityEngine.EventSystems;
+
+	public static class EventTriggerExtensions
+	{
+		public static void AddEventTriggerListener (this EventTrigger trigger,
+		                                            EventTriggerType eventType,
+		                                            System.Action<BaseEventData> callback)
+		{
+			EventTrigger.Entry entry = new EventTrigger.Entry ();
+			entry.eventID = eventType;
+			entry.callback = new EventTrigger.TriggerEvent ();
+			entry.callback.AddListener (new UnityAction<BaseEventData> (callback));
+			trigger.triggers.Add (entry);
+		}
+	}
+}
