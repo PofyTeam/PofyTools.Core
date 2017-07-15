357b639b8fc15170ac5168b37947b86d0d5f57a8
Studio Guvernal <contact@guvernal.com>
2017-07-15 17:39:03 +0200

    extensions

--- Extensions/ListExtensions.cs
+++ Extensions/ListExtensions.cs
@@ -1,20 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class ListExtensions
+﻿namespace PofyTools
 {
-    /// <summary>
-    /// Gets the random element from the list or element's type default value.
-    /// </summary>
-    /// <returns>The random element or element's type default value.</returns>
-    public static T GetRandom<T>(this List<T>list)
-    {
-        int count = list.Count;
-        T result = default(T);
+using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
 
-        if (count != 0)
-            result = list[Random.Range(0, count - 1)];
-        return result;
+    public static class ListExtensions
+    {
+        /// <summary>
+        /// Gets the random element from the list or element's type default value.
+        /// </summary>
+        /// <returns>The random element or element's type default value.</returns>
+        public static T GetRandom<T>(this List<T>list)
+        {
+            int count = list.Count;
+            T result = default(T);
+
+            if (count != 0)
+                result = list[Random.Range(0, count - 1)];
+            return result;
+        }
     }
 }
---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace PofyTools
+{
+using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using System.Globalization;
+
+    public static class StringExtensions
+    {
+        public static string ToTitle(this string value)
+        {
+            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(value.ToLower());
+        }
+    }
+}
