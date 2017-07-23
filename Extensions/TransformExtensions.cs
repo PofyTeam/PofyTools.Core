6a8f77fd3e457b5cafd43e8f78007d8c47530b1d
Studio Guvernal <contact@guvernal.com>
2017-07-23 15:20:18 +0200

    transform extensions

---
+++ Extensions/TransformExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace PofyTools
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public static class TransformExtensions
+    {
+        public static void ClearChildren(this Transform transform)
+        {
+            Transform child = null;
+            for (int i = transform.childCount - 1; i >= 0; --i)
+            {
+                child = transform.GetChild(i);
+                GameObject.Destroy(child.gameObject);
+            }
+        }
+    }
+}
