6852ae756097f3edeedd9faf656b74bd1f5c6214
Studio Guvernal <contact@guvernal.com>
2017-09-26 19:42:21 +0200

    transform extensions

--- Extensions/TransformExtensions.cs
+++ Extensions/TransformExtensions.cs
@@ -1,19 +1,40 @@
 ﻿namespace PofyTools
 {
-    using System.Collections;
-    using System.Collections.Generic;
     using UnityEngine;
 
     public static class TransformExtensions
     {
-        public static void ClearChildren(this Transform transform)
+        public static void ClearChildren (this Transform transform)
         {
             Transform child = null;
             for (int i = transform.childCount - 1; i >= 0; --i)
             {
-                child = transform.GetChild(i);
-                GameObject.Destroy(child.gameObject);
+                child = transform.GetChild (i);
+                GameObject.Destroy (child.gameObject);
             }
+        }
+
+        public static float ForwardToPointAngle (this Transform transform, Vector3 point)
+        {
+            return Vector3.Angle (transform.forward, (point - transform.position).normalized);
+        }
+
+        public static bool IsPointLeft (this Transform transform, Vector3 point)
+        {
+            Plane directionPlane = new Plane (transform.right, transform.position);
+            return directionPlane.GetSide (point);
+        }
+
+        public static bool IsPointBelow (this Transform transform, Vector3 point)
+        {
+            Plane directionPlane = new Plane (transform.up, transform.position);
+            return directionPlane.GetSide (point);
+        }
+
+        public static bool IsPointBehind (this Transform transform, Vector3 point)
+        {
+            Plane directionPlane = new Plane (transform.forward, transform.position);
+            return directionPlane.GetSide (point);
         }
     }
 }
