60cd07477b057f29d64f788392bcf75b729ce786
Danilo Čoda Vukomanović <dvukomanovic@gmail.com>
2018-02-17 12:33:54 +0100

    Get Next Random

--- Extensions/ArrayExtensions.cs
+++ Extensions/ArrayExtensions.cs
@@ -5,12 +5,36 @@ using UnityEngine;
 public static class ArrayExtensions
 {
 
-    public static T GetRandom<T>(this T[] array)
+    public static T GetRandom<T> (this T[] array)
     {
-        T result = default(T);
+        T result = default (T);
 
         if (array.Length != 0)
-            result = array[Random.Range(0, array.Length)];
+            result = array[Random.Range (0, array.Length)];
         return result;
+    }
+
+    public static T GetNextRandom<T> (this T[] array, ref int lastRandomIndex)
+    {
+        int newIndex = lastRandomIndex;
+        int length = array.Length;
+        Debug.Log ("newIndex is " + newIndex);
+
+        if (length > 1)
+        {
+            do
+            {
+                newIndex = Random.Range (0, length);
+            }
+            while (lastRandomIndex == newIndex);
+        }
+        else
+        {
+            newIndex = 0;
+        }
+
+        lastRandomIndex = newIndex;
+
+        return array[newIndex];
     }
 }
--- Extensions/ListExtensions.cs
+++ Extensions/ListExtensions.cs
@@ -10,14 +10,33 @@
         /// Gets the random element from the list or element's type default value.
         /// </summary>
         /// <returns>The random element or element's type default value.</returns>
-        public static T GetRandom<T>(this List<T>list)
+        public static T GetRandom<T> (this List<T> list)
         {
             int count = list.Count;
-            T result = default(T);
+            T result = default (T);
 
             if (count != 0)
-                result = list[Random.Range(0, count)];
+                result = list[Random.Range (0, count)];
             return result;
+        }
+
+        public static T GetNextRandom<T> (this List<T> list, ref int lastRandomIndex)
+        {
+            int newIndex = lastRandomIndex;
+            int length = list.Count;
+
+            if (length > 1)
+            {
+                do
+                {
+                    newIndex = Random.Range (0, length);
+                }
+                while (lastRandomIndex == newIndex);
+            }
+
+            lastRandomIndex = newIndex;
+
+            return list[newIndex];
         }
     }
 }
